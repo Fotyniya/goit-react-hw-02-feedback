78c1831b912cef937cab16638639af3314710393
Fotyniya <s.kozachenko@ekwo.dp.ua>
2023-02-26 15:41:14 +0200

    create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import { FeedbackOptions } from "./FeedbackOptions";
+import { Statistics } from "./Statistics";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  countGoodFeedback = () =>{
+    
+    this.setState(prevState => { 
+      return {
+        good: prevState.good + 1,
+      }
+    })
+  };
+
+  countBadFeedback = ()=>{
+    this.setState(prevState => { 
+      return {
+        bad: prevState.bad + 1,
+      }
+    })
+  };
+
+  countNeutralFeedback = ()=>{
+    this.setState(prevState => {    
+      return {
+        neutral: prevState.neutral + 1,
+      }
+    });
+  };
+ 
+  onCountTotalFeedback = () => {
+    return this.state.good+this.state.bad+this.state.neutral;
+  }
+  
+  countPositiveFeedbackPercentage = ()=>{
+    return this.onCountTotalFeedback() ? Math.round(this.state.good/this.onCountTotalFeedback()*100) : 0;
+  }
+  
+    render(){
+      return(
+      <div>
+          <FeedbackOptions
+          onGoodFeedback = {this.countGoodFeedback}
+          onNeutralFeedback = {this.countNeutralFeedback}
+          onBadFeedback = {this.countBadFeedback}
+        />
+          <Statistics 
+            good = {this.state.good}
+            bad = {this.state.bad}
+            neutral = {this.state.neutral}
+            total = {this.onCountTotalFeedback()}
+            positivePercentage = {this.countPositiveFeedbackPercentage()}
+          /> 
+      </div>
+      )
+    } 
+}
---
+++ src/components/Notification.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+export class Notification extends Component {
+    render(){
+    return <div>
+        <p>{ this.props.message }</p>
+    </div>}
+} 
